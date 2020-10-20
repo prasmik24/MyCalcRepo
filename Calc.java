63f9109a36abe2e8e0df8a4f0159e8481f992f50
Prashant Kumar Jha <52373062+prasmik24@users.noreply.github.com>
2020-10-20 11:17:51 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -5,10 +5,13 @@ return a+b;
   public static int diff(int a,int b){
 return a-b;
 }
+  public static int prod(int a,int b){
+return a*b;
+}
 public static void main(String args[])
 {
   System.out.println(add(10,20));
   System.out.println(diff(10,20));
-  
+  System.out.println(prod(10,20));
 }
 }
