3113be5574d2809b25746c736143ba8f2984a6ac
Prashant Kumar Jha <52373062+prasmik24@users.noreply.github.com>
2020-10-20 10:56:04 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -2,8 +2,13 @@ class Calc{
 public static int add(int a,int b){
 return a+b;
 }
+  public static int diff(int a,int b){
+return a+b;
+}
 public static void main(String args[])
 {
   System.out.println(add(10,20));
+  System.out.println(diff(10,20));
+  
 }
 }
